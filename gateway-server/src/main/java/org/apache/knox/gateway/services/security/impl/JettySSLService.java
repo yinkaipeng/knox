39ea577b5c448e8cda9f49a0f2a60479a27e64f7
Robert Levas <rlevas@apache.org>
2019-03-05 15:17:21 -0500

    BUG-118389 - KNOX-1801 - Master secret is incorrectly assumed when a custom truststore is not specified when clientauth is enabled (#63)
    Change-Id: Id6ed40ac48f4e1c3b8513eb0a64a513e0a4c7db8

--- gateway-server/src/main/java/org/apache/knox/gateway/services/security/impl/JettySSLService.java
+++ gateway-server/src/main/java/org/apache/knox/gateway/services/security/impl/JettySSLService.java
@@ -17,14 +17,7 @@
  */
 package org.apache.knox.gateway.services.security.impl;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateNotYetValidException;
 import java.security.cert.X509Certificate;
@@ -41,7 +34,6 @@ import org.apache.knox.gateway.services.security.AliasService;
 import org.apache.knox.gateway.services.security.AliasServiceException;
 import org.apache.knox.gateway.services.security.KeystoreService;
 import org.apache.knox.gateway.services.security.KeystoreServiceException;
-import org.apache.knox.gateway.services.security.MasterService;
 import org.apache.knox.gateway.services.security.SSLService;
 import org.apache.knox.gateway.util.X500PrincipalParser;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
@@ -52,21 +44,8 @@ public class JettySSLService implements SSLService {
   private static final String GATEWAY_CREDENTIAL_STORE_NAME = "__gateway";
   private static GatewayMessages log = MessagesFactory.get( GatewayMessages.class );
 
-  private MasterService ms;
   private KeystoreService ks;
   private AliasService as;
-  private List<String> sslIncludeCiphers = null;
-  private List<String> sslExcludeCiphers = null;
-  private List<String> sslExcludeProtocols = null;
-  private boolean clientAuthNeeded;
-  private boolean trustAllCerts;
-  private String truststorePath;
-  private String trustStoreType;
-  private boolean clientAuthWanted;
-
-  public void setMasterService(MasterService ms) {
-    this.ms = ms;
-  }
 
   public void setAliasService(AliasService as) {
     this.as = as;
@@ -95,7 +74,7 @@ public class JettySSLService implements SSLService {
         log.credentialStoreForGatewayFoundNotCreating();
       }
     } catch (KeystoreServiceException e) {
-      throw new ServiceLifecycleException("Keystore was not loaded properly - the provided (or persisted) master secret may not match the password for the keystore.", e);
+      throw new ServiceLifecycleException("Keystore was not loaded properly - the provided password may not match the password for the keystore.", e);
     }
 
     try {
@@ -108,9 +87,6 @@ public class JettySSLService implements SSLService {
         } catch (AliasServiceException e) {
           throw new ServiceLifecycleException("Error accessing credential store for the gateway.", e);
         }
-        if (passphrase == null) {
-          passphrase = ms.getMasterSecret();
-        }
         ks.addSelfSignedCertForGateway(config.getIdentityKeyAlias(), passphrase);
       }
       else {
@@ -120,15 +96,6 @@ public class JettySSLService implements SSLService {
     } catch (KeystoreServiceException e) {
       throw new ServiceLifecycleException("The identity keystore was not loaded properly - the provided password may not match the password for the keystore.", e);
     }
-
-    sslIncludeCiphers = config.getIncludedSSLCiphers();
-    sslExcludeCiphers = config.getExcludedSSLCiphers();
-    sslExcludeProtocols = config.getExcludedSSLProtocols();
-    clientAuthNeeded = config.isClientAuthNeeded();
-    clientAuthWanted = config.isClientAuthWanted();
-    truststorePath = config.getTruststorePath();
-    trustAllCerts = config.getTrustAllCerts();
-    trustStoreType = config.getTruststoreType();
   }
 
   private void logAndValidateCertificate(GatewayConfig config) throws ServiceLifecycleException {
@@ -166,64 +133,75 @@ public class JettySSLService implements SSLService {
     }
   }
 
-  public Object buildSslContextFactory(String keystoreFileName, String keystoreType, String alias) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
-    SslContextFactory sslContextFactory = new SslContextFactory( true );
-    sslContextFactory.setCertAlias( alias );
-    sslContextFactory.setKeyStoreType(keystoreType);
-    sslContextFactory.setKeyStorePath(keystoreFileName);
-    char[] master = ms.getMasterSecret();
+  @Override
+  public Object buildSslContextFactory(GatewayConfig config) throws AliasServiceException {
+    String identityKeystorePath = config.getIdentityKeystorePath();
+    String identityKeystoreType = config.getIdentityKeystoreType();
+    String identityKeyAlias = config.getIdentityKeyAlias();
 
-    char[] keystorePasswordChars = null;
+    SslContextFactory sslContextFactory = new SslContextFactory( true );
+    sslContextFactory.setCertAlias( identityKeyAlias );
+    sslContextFactory.setKeyStoreType(identityKeystoreType);
+    sslContextFactory.setKeyStorePath(identityKeystorePath );
+
+    char[] keystorePasswordChars;
     try {
       keystorePasswordChars = as.getGatewayIdentityKeystorePassword();
     } catch (AliasServiceException e) {
-      log.creatingKeyStoreForGateway();
-      // nop - default passphrase will be used
+      log.failedToGetPasswordForGatewayIdentityKeystore(e);
+      throw e;
     }
-    if(keystorePasswordChars == null) {
-      // If a keystore password was not set, use the master password
-      keystorePasswordChars = master;
+    if(keystorePasswordChars != null) {
+      sslContextFactory.setKeyStorePassword(new String(keystorePasswordChars));
     }
-    sslContextFactory.setKeyStorePassword(new String(keystorePasswordChars));
 
-    char[] keypass = null;
+    char[] keypass;
     try {
       keypass = as.getGatewayIdentityPassphrase();
     } catch (AliasServiceException e) {
-      // nop - default passphrase will be used
+      log.failedToGetPassphraseForGatewayIdentityKey(e);
+      throw e;
     }
-    if (keypass == null) {
-      // there has been no alias created for the key - let's assume it is the same as the keystore password
-      keypass = master;
+    if(keypass != null) {
+      sslContextFactory.setKeyManagerPassword(new String(keypass));
     }
-    sslContextFactory.setKeyManagerPassword(new String(keypass));
 
-    String truststorePassword = null;
+    boolean clientAuthNeeded = config.isClientAuthNeeded();
+    boolean clientAuthWanted = config.isClientAuthWanted();
     if (clientAuthNeeded || clientAuthWanted) {
+      String truststorePath = config.getTruststorePath();
+      String trustStoreType;
+      char[] truststorePassword;
+
       if (truststorePath != null) {
-        char[] truststorePwd = null;
+        trustStoreType = config.getTruststoreType();
+
         try {
-          truststorePwd = as.getPasswordFromAliasForGateway(GATEWAY_TRUSTSTORE_PASSWORD);
+          truststorePassword = as.getPasswordFromAliasForGateway(GATEWAY_TRUSTSTORE_PASSWORD);
         } catch (AliasServiceException e) {
-          // nop - master secret will be used
+          log.failedToGetPasswordForGatewayTruststore(GATEWAY_TRUSTSTORE_PASSWORD, e);
+          throw e;
         }
-        if (truststorePwd != null) {
-          truststorePassword = new String(truststorePwd);
-        }
-        else {
-          truststorePassword = new String(master);
-        }
-        sslContextFactory.setTrustStore(loadKeyStore(truststorePath, trustStoreType, truststorePassword.toCharArray()));
-        sslContextFactory.setTrustStorePassword(truststorePassword);
-        sslContextFactory.setTrustStoreType(trustStoreType);
       }
       else {
         // when clientAuthIsNeeded but no truststore provided
         // default to the server's keystore and details
-        sslContextFactory.setTrustStore(loadKeyStore(keystoreFileName, keystoreType, master));
-        sslContextFactory.setTrustStorePassword(new String(master));
-        sslContextFactory.setTrustStoreType(keystoreType);
+        truststorePath = identityKeystorePath;
+        trustStoreType = identityKeystoreType;
+
+        try {
+          truststorePassword = as.getGatewayIdentityKeystorePassword();
+        } catch (AliasServiceException e) {
+          log.failedToGetPasswordForGatewayTruststore(config.getIdentityKeystorePasswordAlias(), e);
+          throw e;
+        }
       }
+
+      sslContextFactory.setTrustStorePath(truststorePath);
+      if(truststorePassword != null) {
+        sslContextFactory.setTrustStorePassword(new String(truststorePassword));
+      }
+      sslContextFactory.setTrustStoreType(trustStoreType);
     }
     if (clientAuthNeeded) {
       sslContextFactory.setNeedClientAuth( clientAuthNeeded );
@@ -231,13 +209,20 @@ public class JettySSLService implements SSLService {
     else {
       sslContextFactory.setWantClientAuth( clientAuthWanted );
     }
-    sslContextFactory.setTrustAll( trustAllCerts );
+
+    sslContextFactory.setTrustAll( config.getTrustAllCerts() );
+
+    List<String> sslIncludeCiphers = config.getIncludedSSLCiphers();
     if (sslIncludeCiphers != null && !sslIncludeCiphers.isEmpty()) {
       sslContextFactory.setIncludeCipherSuites( sslIncludeCiphers.toArray(new String[sslIncludeCiphers.size()]) );
     }
+
+    List<String> sslExcludeCiphers = config.getExcludedSSLCiphers();
     if (sslExcludeCiphers != null && !sslExcludeCiphers.isEmpty()) {
       sslContextFactory.setExcludeCipherSuites( sslExcludeCiphers.toArray(new String[sslExcludeCiphers.size()]) );
     }
+
+    List<String> sslExcludeProtocols = config.getExcludedSSLProtocols();
     if (sslExcludeProtocols != null && !sslExcludeProtocols.isEmpty()) {
       sslContextFactory.setExcludeProtocols( sslExcludeProtocols.toArray(new String[sslExcludeProtocols.size()]) );
     }
@@ -255,19 +240,4 @@ public class JettySSLService implements SSLService {
     // TODO Auto-generated method stub
 
   }
-
-  private static KeyStore loadKeyStore( String fileName, String storeType, char[] storePass ) throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException {
-    KeyStore keystore = KeyStore.getInstance(storeType);
-    //Coverity CID 1352655
-    InputStream is = new FileInputStream(fileName);
-    try {
-      keystore.load( is, storePass );
-    } finally {
-      if( is != null ) {
-        is.close();
-      }
-    }
-    return keystore;
-  }
-
 }
